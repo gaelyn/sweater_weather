f3f2eedd082d284e9e8b576349d1f72a9a6f9534
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 13:53:37 -0500

    Create fetch forecast method in the service

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,7 +1,11 @@
 class ForecastService
   class << self
+    def fetch_forecast(lat,long)
+      response = conn.get("/data/2.5/onecall?lat=#{lat}&lon=#{long}&exclude=minutely&appid=#{ENV['WEATHER_KEY']}&units=imperial")
+      parse_json(response)
+    end
     private
-    
+
     def conn
       Faraday.new('https://api.openweathermap.org')
     end
