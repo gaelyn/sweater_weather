d67c20576a9b4df651276198e33b0560c389fdc7
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 12:21:29 -0500

    Continue building out forecast index

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = MapFacade.get_lat_long(params[:location])
-    # forecast = ForecastFacade.get_city_forecast(location)
+    require "pry"; binding.pry
+    forecast = ForecastFacade.get_city_forecast(location.lat,location.long)
   end
 end
