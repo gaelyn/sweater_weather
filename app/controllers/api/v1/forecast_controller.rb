becd765d38359e5823993cbc5482edb6667a34ec
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 15:10:05 -0500

    Generate serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = MapFacade.get_lat_long(params[:location])
-    forecast = ForecastFacade.get_cuurent_weather(location.lat,location.long)
-    require "pry"; binding.pry
+    current_weather = ForecastFacade.get_current_weather(location.lat,location.long)
+    render json: current_weather
   end
 end
