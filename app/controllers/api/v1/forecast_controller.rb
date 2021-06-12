d97fb5f9a5dc74fa561de910b4063aedc576ea35
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 14:23:12 -0500

    Rearrage test folders

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = MapFacade.get_lat_long(params[:location])
-    require "pry"; binding.pry
     forecast = ForecastFacade.get_cuurent_weather(location.lat,location.long)
+    require "pry"; binding.pry
   end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'WeatherFacade' do
-  it 'returns forecast objects' do
+  it 'returns forecast objects', :vcr do
     location = MapFacade.get_lat_long('denver,co')
     response = ForecastFacade.get_cuurent_weather(location.lat, location.long)
   end
