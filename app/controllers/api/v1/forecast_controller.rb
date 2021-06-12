bc912550763cc18c8ca240007dbcc3072015b15c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 13:49:36 -0500

    Pivot to multi-method approach to facade

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = MapFacade.get_lat_long(params[:location])
     require "pry"; binding.pry
-    forecast = ForecastFacade.get_city_forecast(location.lat,location.long)
+    forecast = ForecastFacade.get_cuurent_weather(location.lat,location.long)
   end
 end
