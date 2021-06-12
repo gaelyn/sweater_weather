69614078b220b40f7283a1fc4fc2131774a48d6e
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 15:09:39 -0500

    Fix misspelled word

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,6 @@
 class ForecastFacade
   class << self
-    def get_cuurent_weather(lat,long)
+    def get_current_weather(lat,long)
       json = ForecastService.fetch_forecast(lat,long)[:current]
       CurrentWeather.new(json)
     end
