2f8d30e7adb6f9a4c4240444993d029286a9269c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-13 16:40:33 -0500

    Utilize has_secure_token for api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :password, presence: true, confirmation: true
   has_secure_password
+  has_secure_token :api_key
 end
