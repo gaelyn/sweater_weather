89229bbe77e3d7ca5e24a61a538e98f65e88b8c3
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-13 12:26:07 -0500

    Sad path

--- spec/requests/api/v1/backgrounds/index_spec.rb
+++ spec/requests/api/v1/backgrounds/index_spec.rb
@@ -18,8 +18,11 @@ RSpec.describe 'Backgrounds Index' do
     end
   end
 
-  # describe 'Sad Path' do
-  #   it 'shows error if no location given' do
-  #   end
-  # end
+  describe 'Sad Path' do
+    it 'shows error if no location given' do
+      get '/api/v1/backgrounds'
+      expect(response.body).to eq("{\"errors\":\"Location not given\"}")
+      expect(response.status).to eq(400)
+    end
+  end
 end
