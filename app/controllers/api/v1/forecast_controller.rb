223577f90a72080e7702e5fd242f7ee98d1b0804
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 21:17:16 -0500

    Add sad path

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    location = MapFacade.get_lat_long(params[:location])
-    current_weather = ForecastFacade.get_local_weather(location.lat,location.long)
-    render json: ForecastSerializer.new(current_weather)
+    if params[:location]
+      location = MapFacade.get_lat_long(params[:location])
+      current_weather = ForecastFacade.get_local_weather(location.lat,location.long)
+      render json: ForecastSerializer.new(current_weather)
+    else
+      render json: { errors: 'Location not given' }, status: :bad_request
+    end
   end
 end
