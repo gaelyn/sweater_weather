10b57d0bfc14afc64ece841ea0091da2cb061cb9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-12 21:41:03 -0500

    Add files, need tests

---
+++ spec/poros/daily_weather_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Daily Weather' do
+  describe 'encapsulates and returns daily weather' do
+    it 'shows daily weather attributes' do
+      
+    end
+  end
+end
---
+++ spec/poros/hourly_weather_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Hourly Weather' do
+  describe 'encapsulates and returns hourly weather' do
+    it 'shows hourly weather attributes' do
+
+    end
+  end
+end
