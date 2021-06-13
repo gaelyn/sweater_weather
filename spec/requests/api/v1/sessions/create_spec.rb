a657f737678672f78f227c5d1add796fb33ec678
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-13 17:38:18 -0500

    Create spec

---
+++ spec/requests/api/v1/sessions/create_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Sessions Create' do
+  before :each do
+    @user =
+  end
+
+  describe 'Happy Path' do
+    it 'can create a new session when user logs in' do
+      
+    end
+  end
+end
