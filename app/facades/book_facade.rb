6fe226821d009309eda4b33b087c3400ed73c0f9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 11:54:49 -0500

    Add forecast to book poro and serializer

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,9 +1,9 @@
 class BookFacade
   class << self
-    def get_books_by_location(location, quantity)
+    def get_books_by_location(location, quantity, forecast)
       city_state = location.split(",")
       json = BookService.fetch_books(city_state, quantity)
-      Book.new(location, json[:numFound], json[:docs])
+      Book.new(location, json[:numFound], json[:docs], forecast)
     end
   end
 end
--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,9 +1,10 @@
 class Book
   attr_reader :id, :destination, :total_books_found
-  def initialize(location, results, books)
+  def initialize(location, results, books, forecast)
     @id = nil
     @destination = location
     @total_books_found = results
+    @forecast = forecast
     @books = books
   end
 
@@ -15,5 +16,12 @@ class Book
         publisher: book[:publisher]
       }
     end
+  end
+
+  def forecast
+    {
+      summary: @forecast.conditions,
+      temperature: "#{@forecast.temperature} F"
+    }
   end
 end
