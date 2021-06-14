c031bfd5620369879bccd9a2ee27cdcbd5e64f89
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 11:53:55 -0500

    Add forecast and location back into controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::BooksController < ApplicationController
   def index
     location = MapFacade.get_lat_long(params[:location])
-    forecast = ForecastFacade.get_local_weather(location.lat,location.long)
-    require "pry"; binding.pry
-    books = BookFacade.get_books_by_location(params[:location], params[:quantity])
+    forecast = ForecastFacade.get_local_weather(location.lat,location.long).current_weather
+    books = BookFacade.get_books_by_location(params[:location], params[:quantity], forecast)
     render json: BooksSerializer.new(books)
   end
 end
--- spec/requests/api/v1/book_search_spec.rb
+++ spec/requests/api/v1/book_search_spec.rb
@@ -5,9 +5,8 @@ RSpec.describe 'Book Search' do
     it 'can return books about given destination' do
       get '/api/v1/book-search', params: { location: "denver,co", quantity: 5 }
       books = JSON.parse(response.body, symbolize_names: true)
-      expect(resonse).to be_successful
+      expect(response).to be_successful
       expect(response.status).to eq(200)
-      require "pry"; binding.pry
 
     end
   end
