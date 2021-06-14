3e27f32f877ad80f56d6de510d2dfc61692206e3
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 11:07:22 -0500

    Continue work on book facade to get desired info

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,9 +1,11 @@
 class BookFacade
   class << self
-    def get_books_by_location(location, qty)
+    def get_books_by_location(location, quantity)
       city_state = location.split(",")
-      json = BookService.fetch_books(city_state, qty)
-      # Book.new(json)
+      json = BookService.fetch_books(city_state, quantity)
+      json[:docs].map do |book|
+        Book.new(location, json[:numFound], book)
+      end
     end
   end
 end
