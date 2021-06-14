e24de3a7b827a10ccf4ca73ce3618f337050a927
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 11:24:45 -0500

    Finish testing facade

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -3,9 +3,7 @@ class BookFacade
     def get_books_by_location(location, quantity)
       city_state = location.split(",")
       json = BookService.fetch_books(city_state, quantity)
-      json[:docs].map do |book|
-        Book.new(location, json[:numFound], book)
-      end
+      Book.new(location, json[:numFound], json[:docs])
     end
   end
 end
