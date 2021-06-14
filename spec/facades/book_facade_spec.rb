9fc9aeda9b5fc51a6410cd09e23411d2fb56b8d8
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 12:10:58 -0500

    Add forecast to tests

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Book Facade' do
   it 'returns books queried by location' do
-    books = BookFacade.get_books_by_location("denver,co", 5)
+    current = {
+               :dt=>1623522198,
+               :sunrise=>1623497489,
+               :sunset=>1623551306,
+               :temp=>82.89,
+               :feels_like=>80.82,
+               :pressure=>1012,
+               :humidity=>26,
+               :dew_point=>44.53,
+               :uvi=>10.67,
+               :clouds=>0,
+               :visibility=>10000,
+               :wind_speed=>1.01,
+               :wind_deg=>334,
+               :wind_gust=>3,
+               :weather=>[{:id=>800, :main=>"Clear", :description=>"clear sky", :icon=>"01d"}]
+             }
+    current_weather = CurrentWeather.new(current)
+    books = BookFacade.get_books_by_location("denver,co", 5, current_weather)
     expect(books.destination).to eq("denver,co")
     expect(books.total_books_found).to eq(606)
     expect(books.books).to be_an(Array)
     expect(books.books.first).to be_a(Hash)
     expect(books.books.count).to eq(5)
+    expect(books.forecast).to be_a(Hash)
   end
 end
--- spec/poros/book_spec.rb
+++ spec/poros/book_spec.rb
@@ -3,6 +3,24 @@ require 'rails_helper'
 RSpec.describe 'Book' do
   describe 'it encapsulates and returns books' do
     it 'returns a book objects with attributes' do
+      current = {
+                 :dt=>1623522198,
+                 :sunrise=>1623497489,
+                 :sunset=>1623551306,
+                 :temp=>82.89,
+                 :feels_like=>80.82,
+                 :pressure=>1012,
+                 :humidity=>26,
+                 :dew_point=>44.53,
+                 :uvi=>10.67,
+                 :clouds=>0,
+                 :visibility=>10000,
+                 :wind_speed=>1.01,
+                 :wind_deg=>334,
+                 :wind_gust=>3,
+                 :weather=>[{:id=>800, :main=>"Clear", :description=>"clear sky", :icon=>"01d"}]
+               }
+
       books = [
         {
           isbn: ["12345", "678910"],
@@ -11,10 +29,14 @@ RSpec.describe 'Book' do
         }
       ]
 
-      denver_books = Book.new("denver,co", 1, books)
+      current_weather = CurrentWeather.new(current)
+      denver_books = Book.new("denver,co", 1, books, current_weather)
       expect(denver_books.destination).to eq("denver,co")
       expect(denver_books.total_books_found).to eq(1)
       expect(denver_books.books).to be_an(Array)
+      expect(denver_books.forecast).to be_a(Hash)
+      expect(denver_books.forecast[:summary]).to eq("clear sky")
+      expect(denver_books.forecast[:temperature]).to eq("82.89 F")
     end
   end
 end
