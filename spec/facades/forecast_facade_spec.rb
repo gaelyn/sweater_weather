82c0f3b3c27b31598f120a3c558f27deb825c04a
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-15 10:36:18 -0500

    Create method to get 48 hour weather for trips

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe 'WeatherFacade' do
     expect(response.hourly_weather).to be_an(Array)
     expect(response.hourly_weather.first).to be_a(HourlyWeather)
   end
+
+  it 'returns Hourly Weather objects for trip', :vcr do
+    location = MapFacade.get_lat_long('Los Angeles, CA')
+    response = ForecastFacade.get_hourly_weather_for_trip(location.lat, location.long)
+    expect(response).to be_an(Array)
+    expect(response.first).to be_a(Hash)
+    expect(response.count).to eq(48)
+  end
 end
