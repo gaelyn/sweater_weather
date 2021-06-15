8a53ceb983c524b7fc3b4c6623f01ef638c99057
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 19:40:08 -0500

    Change RoadTripSerializer to RoadtripSerializer

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     trip = MapFacade.get_road_trip(params[:origin], params[:destination])
-    render json: RaodTripSerializer.new(trip)
+    render json: RoadtripSerializer.new(trip)
   end
 end
