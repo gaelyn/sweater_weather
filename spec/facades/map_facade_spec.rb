eadbf1c63857f8ff1a527b666654229299b85490
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 21:54:22 -0500

    Test forecast method and add vcr

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -6,13 +6,16 @@ RSpec.describe 'Map Facade' do
     expect(location).to be_a(Location)
   end
 
-  it 'returns a route between two given destinations' do
+  it 'returns a route between two given destinations', :vcr do
     route = MapFacade.get_road_trip("Denver,CO", "Pueblo,CO")
     expect(route).to be_a(Route)
     expect(route.weather_at_eta).to be_a(Hash)
   end
 
-  xit 'returns hourly forecast for destination' do
+  it 'returns hourly forecast for destination', :vcr do
     forecast = MapFacade.forecast("Pueblo,CO")
+    expect(forecast).to be_an(Array)
+    expect(forecast.first).to be_an(HourlyWeather)
+    expect(forecast.count).to eq(8)
   end
 end
