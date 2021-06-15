419ca61cc7a5419e3ade8922122227502cf489e4
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-15 17:01:30 -0500

    Refactor test

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Image' do
       parsed = JSON.parse(json, symbolize_names: true)
       denver = parsed[:results].first
       denver[:location] = "denver,co"
+
       image = Image.new(denver)
       expect(image.location).to eq("denver,co")
       expect(image.image_url).to eq("https://images.unsplash.com/photo-1619856699906-09e1f58c98b1?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMzkyNjF8MHwxfHNlYXJjaHwxfHxkZW52ZXIlMkNjb3xlbnwwfHx8fDE2MjM3OTM3MzU&ixlib=rb-1.2.1&q=85")
