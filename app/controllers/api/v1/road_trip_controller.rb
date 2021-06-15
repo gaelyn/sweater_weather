203279bffc4974c1d77f3b95b7ac9d82dbb812ef
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 19:29:42 -0500

    call map facade in index to get route

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    trip = RaodTripFacade.get_road_trip(params[:origin], params[:destination])
+    trip = MapFacade.get_road_trip(params[:origin], params[:destination])
     render json: RaodTripSerializer.new(trip)
-    require "pry"; binding.pry
   end
-
-  # private
-  #
-  # def _params
-  #   params.permit(:)
-  # end
 end
