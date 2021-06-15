8723922cec0c0ae6b41d43143ee15d72b05e8cc3
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 20:22:20 -0500

    Add arrival forecast to route

--- app/poros/route.rb
+++ app/poros/route.rb
@@ -1,12 +1,19 @@
 class Route
-  attr_reader :id, :start_city, :end_city, :travel_time
-  def initialize(data)
+  attr_reader :id, :start_city, :end_city, :travel_time, :hours_to_arrival
+  def initialize(data, forecast)
     @id = nil
     @start_city = "#{data[:locations][0][:adminArea5]},#{data[:locations][0][:adminArea3]}"
     @end_city = "#{data[:locations][1][:adminArea5]},#{data[:locations][1][:adminArea3]}"
     @travel_time = Time.at(data[:realTime]).utc.strftime("%H hour(s) and %M minutes")
-    # @weather_at_eta = HourlyWeather.new()
+    @hours_to_arrival = Time.at(data[:realTime]).utc.strftime("%H").to_i
+    @weather_at_eta = forecast
+  end
+
+  def weather_at_eta
+    hourly_weather = @weather_at_eta[@hours_to_arrival]
+    {
+      temperature: hourly_weather.temperature,
+      conditions: hourly_weather.conditions
+    }
   end
 end
-# hours = Time.at(json[:realTime]).utc.strftime("%H").to_i
-# travel time = Time.at(json[:realTime]).utc.strftime("%H hour(s) and %M minutes")
---
+++ spec/poros/route_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Route' do
+  describe 'it encapsulates and returns route info' do
+    # it 'returns the route object attributes' do
+    #
+    #   trip = Route.new(trip_data, forecast)
+    #   expect(trip.).to eq()
+    #   expect(trip.).to eq()
+    # end
+  end
+end
