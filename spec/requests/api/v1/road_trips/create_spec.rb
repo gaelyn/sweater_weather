89f7d6be71f888741e3ed21baa0895f533198162
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-14 19:40:24 -0500

    Add expectations

--- spec/requests/api/v1/road_trips/create_spec.rb
+++ spec/requests/api/v1/road_trips/create_spec.rb
@@ -14,8 +14,16 @@ RSpec.describe 'Road Trip Create' do
         "api_key": "jgn983hy48thw9begh98h4539h4"
       }
       post '/api/v1/road_trip', headers: headers, params: body.to_json
+      trip = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
       expect(response.status).to eq(200)
+      expect(trip[:data][:attributes]).to have_key(:start_city)
+      expect(trip[:data][:attributes]).to have_key(:end_city)
+      expect(trip[:data][:attributes]).to have_key(:travel_time)
+      # expect(trip[:data][:attributes]).to have_key(:weather_at_eta)
+      # expect(trip[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+      # expect(trip[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+      # expect(trip[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
     end
   end
 end
