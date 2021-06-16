2a08361e2cf99374f41ef933d1b2e51433885727
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-15 19:07:31 -0500

    Add vcr

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe "Map Service" do
   end
 
   describe 'get road trip info' do
-    it 'can return data about a route between two points' do
+    it 'can return data about a route between two points', :vcr do
       results = MapService.fetch_route("Denver,CO", "Pueblo,CO")
       expect(results).to be_a(Hash)
       expect(results).to have_key(:route)
     end
 
-    it 'can return data for a very long trip' do
+    it 'can return data for a very long trip', :vcr do
       results = MapService.fetch_route("New York,NY", "Los Angeles,CA")
       expect(results).to be_a(Hash)
       expect(results).to have_key(:route)
